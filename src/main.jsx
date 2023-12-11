622f9983891136864afbe19285a82f04bc47e82f
Ryan <ryan02ff@gmail.com>
2023-12-11 16:56:30 -0300

    creating and styling home page

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
 import GlobalStyles from './styles/global'
 
+import { Home } from './pages/Home'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 100vh;
+
+  display: grid;
+  grid-template-rows: 10.5rem auto;
+  grid-template-areas: 'header' 'content';
+`
