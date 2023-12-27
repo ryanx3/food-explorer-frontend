1bf8a74143ed7d6d8f6b0e83671fc1e3c75a590d
Ryan <ryan02ff@gmail.com>
2023-12-27 18:05:33 -0300

    create page edit

---
+++ src/pages/Edit/index.jsx
@@ -0,0 +1,68 @@
+import { PiCaretLeft } from "react-icons/pi";
+
+import * as Tag from '../../components/Tag'
+import * as Input from "../../components/Input";
+import { Select } from '../../components/Select'
+import { Header } from '../../components/Header'
+import { Footer } from '../../components/Footer'
+import { Button } from '../../components/Button'
+import { Textarea } from '../../components/Textarea'
+
+
+import { Container, Content, FormField, Buttons } from './styles'
+
+export function Edit({ isAdmin = true }) {
+  return (
+    <Container>
+      <Header />
+
+      <main>
+        <Content>
+
+          <a href="#"><PiCaretLeft /> voltar</a>
+          <h1>Adicionar prato</h1>
+
+          <FormField>
+
+            <div className="first-column">
+
+              <Input.Picture title="Imagem do prato" />
+              <Input.Root
+                title="Nome"
+                placeholder="Exemplo: Salada Caesar" />
+              <Select title="Categorias" />
+
+            </div>
+
+            <div className="second-column">
+
+              <Input.Background title="Ingredientes">
+                <Tag.Created title="Pão Naan" />
+                <Tag.New />
+              </Input.Background>
+
+              <Input.Root title="Preço"
+                placeholder="R$ 00,00" />
+
+            </div>
+
+            <Textarea
+              placeholder="Fale brevemente sobre o prato, seus ingredientes e composição."
+              title="Descrição"
+            />
+
+            <Buttons>
+
+              <Button className="delete-button" title="Excluir prato" />
+              <Button className="save-button"  title="Salvar alterações"/>
+
+            </Buttons>
+
+          </FormField>
+        </Content>
+      </main>
+
+      <Footer />
+    </Container>
+  );
+}
---
+++ src/pages/Edit/styles.js
@@ -0,0 +1,101 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  overflow-x: hidden;
+
+  display: grid;
+  grid-template-rows: 10.5rem auto 7.7rem;
+  grid-template-areas: 
+    'header'
+    'content'
+    'footer';
+`
+
+export const Content = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    grid-area: content;
+    margin: 4rem 12.3rem 4rem 12.3rem;
+    
+    > a {
+      display: flex;
+      color: ${({ theme }) => theme.COLORS.LIGHT_300};
+      width: fit-content;
+
+      font-family: inherit;
+      font-size: 2.4rem;
+      font-weight: 700;
+      line-height: 140%;
+
+      svg {
+        font-size: 3.2rem;
+      }
+    }
+
+    > h1 {
+      font-family: inherit;
+      font-size: 3.2rem;
+      font-style: normal;
+      font-weight: 500;
+      line-height: 140%;
+      color: ${({ theme }) => theme.COLORS.LIGHT_300};
+      margin: 2.4rem 0 3.2rem;
+    }
+`
+
+export const FormField = styled.form`
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
+
+  
+  .first-column {
+    display: grid;
+    gap: 32px;
+    grid-template-columns: 23rem 1fr 36.5rem;
+
+    input {
+      height: 24px;
+    }
+  }
+
+  .second-column {
+    display: grid;
+    grid-template-columns: 1fr 300px;
+    grid-column-gap: 3.2rem;
+
+    input {
+      height: 2.4rem;
+    }
+  }
+
+`;
+
+export const Buttons = styled.div`
+  display: flex;
+  width: fit-content;
+  align-self: flex-end;
+  gap: 3.2rem;
+
+  .save-button {
+    transition: 500ms ease-out;
+    background-color: ${({ theme }) => theme.COLORS.TOMATO_400};
+
+    &:hover {
+      transition: 500ms ease-out;
+      background-color: ${({ theme }) => theme.COLORS.TOMATO_100};
+    }
+  }
+
+  .delete-button {
+    transition: 500ms ease-out;
+    background-color: ${({ theme }) => theme.COLORS.DARK_900};
+  }
+`
+
+
