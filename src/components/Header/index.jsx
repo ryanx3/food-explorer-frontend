90c1fdba6f718bcccb75bb9b3e02f7871ed20158
Ryan <ryan02ff@gmail.com>
2023-12-30 22:22:51 -0300

    mobile first header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,32 +1,47 @@
-import { PiSignOut, PiMagnifyingGlassLight, PiReceipt } from "react-icons/pi";
+import { PiSignOut, PiMagnifyingGlassLight, PiReceipt, PiList } from "react-icons/pi";
 
 import { Search } from "../Search";
 import { Button } from "../Button";
 
 import { Brand } from '../../assets/brand' 
 import { BrandAdmin } from '../../assets/brand-admin' 
+import { BrandMobile } from '../../assets/brand-mobile' 
+import { BrandMobileAdmin } from '../../assets/brand-mobile-admin' 
 
-import { Container } from './styles'
+import { Container, Menu, Logo } from './styles'
+import { useMediaQuery } from "react-responsive";
 
 export function Header({isAdmin = false, ...rest }) {
-  const logo = !isAdmin ? <Brand/> : <BrandAdmin/>
+  const isMobile = useMediaQuery({maxWidth: 1024})
+
+  const LogoDesktop = !isAdmin ? <Brand/> : <BrandAdmin/>
+  const LogoMobile = !isAdmin ? <BrandMobile/> : <BrandMobileAdmin/>
 
   return (
     <Container {...rest}>
 
-      { logo }
+      {isMobile && 
+      <Menu>
+        <PiList/>
+      </Menu>
+      }
 
-      <Search 
+    <Logo>
+      { isMobile ? LogoMobile : LogoDesktop}
+    </Logo>
+
+      {!isMobile && <Search 
       placeholder="Busque por pratos ou ingredientes"
       icon={PiMagnifyingGlassLight}
-      />
+      />}
 
-      <Button
+      {!isMobile && <Button
+      className="order-button"
       title={'Pedidos (0)'}
       icon={PiReceipt}
-      /> 
+      /> }
 
-      <PiSignOut/>
+      {isMobile ? <PiReceipt/> : <PiSignOut/>}
     </Container>
   )
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,24 +1,57 @@
 import styled from "styled-components";
 
 export const Container = styled.header`
+  margin: 0 auto;
+  max-width: 100vw;
   width: 100%;
+  padding: 0 10%;
   
-  display: grid;
-  grid-template-columns: 197px 1fr 216px 32px;
+  display: flex;
+  justify-content: space-between;
   align-items: center;
   gap: 3.2rem;
-  
-  padding: 2.4rem 12.3rem;
+
   grid-area: header;
   
   background-color: ${({ theme }) => theme.COLORS.DARK_700};
   
+  .order-button {
+    width: 21.6rem;
+  }
+  
   > svg {
     font-size: 3.2rem;
+    min-width: 3.2rem;
     fill: ${({ theme }) => theme.COLORS.LIGHT_100};
+    cursor: pointer;
+  }
 
-    &:hover {
-      cursor: pointer;
+  @media (max-width: 768px) {
+    padding: 6rem 10% 2.8rem;
+
+    > svg { 
+      font-size: 2.4rem;
     }
+  }
+`
+
+export const Menu = styled.div`
+  svg {
+    color: ${({ theme }) => theme.COLORS.LIGHT_100};
+    font-size: 3.2rem;
+  }
+
+  @media (max-width: 768px) {
+    svg { 
+      font-size: 2.4rem;
+    }
+  }
+`
+
+export const Logo = styled.div`
+    display: flex;
+    min-width: fit-content;
+  @media (max-width: 768px) {
+    min-width: 13.6rem;
   }
 `
