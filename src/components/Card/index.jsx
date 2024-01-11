91fd29b60a01c89ccec296f4b991dd87ac97f605
Ryan <ryan02ff@gmail.com>
2024-01-11 12:07:08 -0300

    card component responsiveness adjustments

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,54 +1,73 @@
-import { PiHeartStraightBold, PiHeartStraightFill, PiCaretRightBold, PiPencilSimple } from "react-icons/pi";
-import { VscHeart, VscHeartFilled } from "react-icons/vsc";
-
-import { Counter } from '../Counter'
-
-import { Container, Title, Description, Order } from "./styles";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  PiHeartStraightBold,
+  PiHeartStraightFill,
+  PiCaretRightBold,
+  PiPencilSimple
+} from "react-icons/pi";
+
+//Components
+import { Counter } from '../Counter';
 import { Button } from "../Button";
 
+import { Container, Picture, Title, Description, Order } from "./styles";
+
 export function Card({ data, isAdmin = false, ...rest }) {
-  const [favorite, setFavorites] = useState(false)
+  const navigate = useNavigate();
+  const [favorite, setFavorites] = useState(false);
 
   function handleIsFavorite() {
     setFavorites(true);
   }
 
+  function handleOpenDetails() {
+    navigate("/details");
+  }
+
+  function handleAdminEdit() {
+      navigate("/edit");
+  }
+
   function handleRemoveFavorite() {
-    setFavorites(false)
+    setFavorites(false);
   }
 
   return (
-    <Container {...rest}>
+    <Container isAdmin={isAdmin} {...rest}>
+      {!isAdmin ? (
+        favorite ? (
+          <PiHeartStraightFill onClick={handleRemoveFavorite} />
+        ) : (
+          <PiHeartStraightBold onClick={handleIsFavorite} />
+        )
+      ) : (
+        <PiPencilSimple onClick={handleAdminEdit}/>
+      )}
 
-      {!isAdmin ?
-        (favorite ?
-          (<PiHeartStraightFill fill="white" onClick={handleRemoveFavorite} />)
-          : (<PiHeartStraightBold onClick={handleIsFavorite} />))
-        : (<PiPencilSimple />)}
+      <Picture onClick={handleOpenDetails}>
+        <img
+          src={data.image}
+          alt={`Image of dish ${data.name}`}
+        />
+      </Picture>
 
-
-      <img src={data.image} alt={`Image of dish ${data.name}`} />
-
-      <Title>
+      <Title onClick={handleOpenDetails}>
         <h2>{data.name}</h2>
         <PiCaretRightBold />
       </Title>
 
       <Description>
         <p>{data.description}</p>
-
         <h3>R$ {data.price}</h3>
-
       </Description>
 
-      {!isAdmin &&
+      {!isAdmin && 
         <Order>
           <Counter />
           <Button title="incluir" />
         </Order>
       }
-
     </Container>
   );
 }
--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -1,24 +1,18 @@
-
 import styled from "styled-components";
 
 export const Container = styled.div`
   width: 30.4rem;
   height: auto;
-
   position: relative;
   display: flex;
   align-items: center;
   flex-direction: column;
   text-align: center;
-
   overflow: hidden;
-
   padding: 2.4rem;
   gap: 1.5rem;
-
-  background-color: ${({ theme }) => theme.COLORS.DARK_900};
+  background-color: ${({ theme }) => theme.COLORS.DARK_300};
   color: ${({ theme }) => theme.COLORS.LIGHT_300};
-  
   border: 1px solid ${({ theme }) => theme.COLORS.DARK_400};
   border-radius: 0.8rem;
 
@@ -27,70 +21,60 @@ export const Container = styled.div`
     top: 1.6rem;
     right: 1.6rem;
     font-size: 2.4rem;
+    fill: ${({ theme }) => theme.COLORS.LIGHT_100};;
     cursor: pointer;
   }
 
+  @media (max-width: 768px) {
+    width: 21rem;
+    height: auto;
+    gap: 1.2rem;
+  }
+`;
+
+export const Picture = styled.div`
   img {
     width: 17.6rem;
     height: auto;
     flex-shrink: 0;
-  }
+    cursor: pointer;
 
     @media (max-width: 768px) {
-      width: 21rem;
-      height: auto;
-      gap: 1.2rem;
-
-      img {
-        width:8.8rem;
-       
-      }
+        width: 8.8rem;
+    }
   }
+
 `
 
 export const Title = styled.div`
-    display: flex;
-    align-items: center;
-
+  display: flex;
+  align-items: center;
+  
   h2 {
     max-width: calc(16 * 1ch);
-
     white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
-
     font-size: 2.4rem;
     font-style: inherit;
     font-weight: 700;
     color: ${({ theme }) => theme.COLORS.LIGHT_300};
-  }
-  
-  svg {
-    font-size: 20px;
     cursor: pointer;
   }
 
-
-  @media (max-width: 768px) {
-      h2 {
-        font-family: Poppins;
-        font-size: 1.4rem;
-        font-style: normal;
-        font-weight: 500;
-        line-height: 24px;
-      }
+  svg {
+    font-size: 2.4rem;
+    cursor: pointer;
   }
-`;
-
-export const Order = styled.div`
-  display: flex;
-  gap: 1.6rem;
-
 
   @media (max-width: 768px) {
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
+    h2 {
+      font-family: Poppins;
+      font-size: 1.4rem;
+      font-style: normal;
+      font-weight: 500;
+      line-height: 24px;
+    }
   }
 `;
 
@@ -104,13 +88,11 @@ export const Description = styled.div`
     display: -webkit-box;
     -webkit-line-clamp: 2;
     -webkit-box-orient: vertical;
-
     color: ${({ theme }) => theme.COLORS.LIGHT_400};
     font-family: Roboto;
     font-size: 1.4rem;
     line-height: 160%;
     text-overflow: ellipsis;
-
     min-height: 4.1rem;
     word-break: break-word;
     overflow: hidden;
@@ -124,11 +106,9 @@ export const Description = styled.div`
     font-size: 3.2rem;
     line-height: 160%;
     font-weight: 400;
-
     white-space: nowrap;
     text-overflow: ellipsis;
     overflow: hidden;
-
     max-width: calc(12 * 1ch);
   }
 
@@ -136,8 +116,24 @@ export const Description = styled.div`
     p {
       display: none;
     }
-      h3 {
-        font-size: 1.6rem;
-      }
+    h3 {
+      font-size: 1.6rem;
+    }
   }
-`
+`;
+
+export const Order = styled.div`
+  display: flex;
+  width: 100%;
+  gap: 1.6rem;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    button:nth-child(2) {
+      max-height: 3.2rem;
+    }
+  }
+`;
