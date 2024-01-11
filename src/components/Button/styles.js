fe741c9950f78fd221b5f50731d56d797f66d9ab
Ryan <ryan02ff@gmail.com>
2024-01-11 12:07:24 -0300

    responsiveness adjustments button

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -4,24 +4,24 @@ export const Container = styled.button`
   width: 100%;
 
   display: flex;
-  align-items: center; 
-  justify-content: center;  
+  align-items: center;
+  justify-content: center;
 
-  gap: .8rem;
+  gap: 0.8rem;
   padding: 1.2rem 3.2rem;
-  border-radius: .5rem;
+  border-radius: 0.5rem;
 
   font-family: inherit;
   font-weight: 500;
   font-size: 1.4rem;
   line-height: 2.4rem;
-  
+
   color: ${({ theme }) => theme.COLORS.LIGHT_100};
   background-color: ${({ theme }) => theme.COLORS.TOMATO_100};
 
   white-space: nowrap;
   border: none;
-    
+
   > svg {
     font-size: 3.2rem;
   }
