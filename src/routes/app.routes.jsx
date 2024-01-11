719d87b488698498a83ea2f0cdc2c24cecd601b4
Ryan <ryan02ff@gmail.com>
2024-01-11 12:07:51 -0300

    create routes auth and app

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -4,19 +4,17 @@ import { Home } from '../pages/Home'
 import { DishDetails } from '../pages/DishDetails'
 import { New } from '../pages/New'
 import { Edit } from '../pages/Edit'
-import { SignIn } from '../pages/SignIn'
-import { SignUp } from '../pages/SignUp'
+import { Profile } from '../pages/Profile'
 
 export function AppRoutes() {
   return(
     <Routes>
       <Route element={<Home/>} path="/"/>
       <Route element={<DishDetails/>} path="/details"/>
+      <Route element={<Profile/>} path="/profile"/>
       <Route element={<New/>} path="/new"/>
       <Route element={<Edit/>} path="/edit"/>
-      <Route element={<SignIn/>} path="/login"/>
-      <Route element={<SignUp/>} path="/register"/>
-
+    
       <Route path="*" element={<Navigate to="/"/>}/>
     </Routes>
   )
---
+++ src/routes/auth.routes.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import { SignIn } from '../pages/SignIn'
+import { SignUp } from '../pages/SignUp'
+
+export function AppRoutes() {
+  return(
+    <Routes>
+      <Route element={<SignIn/>} path="/login"/>
+      <Route element={<SignUp/>} path="/register"/>
+
+      <Route path="*" element={<Navigate to="/"/>}/>
+    </Routes>
+  )
+}
