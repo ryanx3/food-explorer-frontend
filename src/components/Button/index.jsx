74204e9461a35951e312213282e390cb06fe0d46
Ryan <ryangabriel_cap@hotmail.com>
2024-02-02 11:50:30 -0300

    "

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,7 +2,7 @@ import { Container } from './styles'
 
 export function Button({ icon: Icon, title, ...rest }) {
   return(
-    <Container {...rest}>
+    <Container {...rest} type='button'>
       {Icon && <Icon size={32}/>}
       {title}
     </Container>
--- src/routes/auth.routes.jsx
+++ src/routes/auth.routes.jsx
@@ -3,10 +3,10 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import { SignIn } from '../pages/SignIn'
 import { SignUp } from '../pages/SignUp'
 
-export function AppRoutes() {
+export function AuthRoutes() {
   return(
     <Routes>
-      <Route element={<SignIn/>} path="/login"/>
+      <Route element={<SignIn/>} path="/"/>
       <Route element={<SignUp/>} path="/register"/>
 
       <Route path="*" element={<Navigate to="/"/>}/>
