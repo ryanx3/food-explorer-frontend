5cc24c271520be3ee0b4fad82f8c917fabe1c7a5
Ryan <ryangabriel_cap@hotmail.com>
2024-02-02 11:54:43 -0300

    putting the AuthProvider around the routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { ThemeProvider } from 'styled-components'
-import theme from './styles/theme'
-import GlobalStyles from './styles/global'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
+import theme from "./styles/theme";
+import GlobalStyles from "./styles/global";
 
-import { Routes } from './routes'
+import { Routes } from "./routes";
+import { ToastContainer } from "react-toastify";
+import { AuthProvider } from "./hooks/Auth";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <ToastContainer
+      position="top-right"
+      autoClose={2000}
+      limit={3}
+      hideProgressBar={true}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="dark"
+      transition:Slide
+    />
     <ThemeProvider theme={theme}>
       <GlobalStyles />
+      <AuthProvider>
         <Routes />
+      </AuthProvider>
     </ThemeProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
