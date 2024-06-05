1dcc5e2ee07d55b70c89ea622eeeef60034f0a37
Ryan <ryangabriel_cap@hotmail.com>
2024-06-05 22:56:47 +0100

    feat: Fechando SideMenu ao pesquisar no mobile

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,18 +1,22 @@
 import { Input } from "../Inputs/Input";
 import { BsSearchHeart, BsArrowLeftShort } from "react-icons/bs";
+
 import { useSearch } from "../../hooks/Search";
+import { useNavigate } from "react-router-dom";
+import { useSideMenu } from "../../hooks/SideMenu";
 
 import { SearchContainer } from "./styles";
-import { useNavigate } from "react-router-dom";
 
 export function Search({ ...rest }) {
   const RedirectTo = useNavigate();
   const { search, setSearch } = useSearch();
+  const { setIsMenuOpen } = useSideMenu()
 
   function executeSearchOnEnter(e) {
     const valueInSearch = e.target.value.trim();
     if (e.key === "Enter") {
       setSearch(valueInSearch);
+      setIsMenuOpen(false)
       RedirectTo("/");
     } else if (valueInSearch === "") {
       return;
