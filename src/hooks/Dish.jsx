e80af1d8d23e44595cc35d932897697fef7a1a59
Ryan <ryangabriel_cap@hotmail.com>
2024-06-11 22:06:06 +0100

    refactory: Refatorando hook do dish

--- src/hooks/Dish.jsx
+++ src/hooks/Dish.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import { api } from "../services/api";
 
@@ -6,36 +6,22 @@ const DishContext = createContext();
 
 function DishProvider({ children }) {
   const [dish, setDish] = useState(null);
-  const [ingredientsExists, setIngredientsExists] = useState([]);
-  const [category, setCategory] = useState("");
 
   async function fetchDishDetails(id) {
     try {
       const response = await api.get(`/dishes/${id}`);
       setDish(response.data);
-      setCategory(response.data.category);
-      setIngredientsExists(response.data.ingredients);
     } catch (error) {
-      if (error.response) {
-        toast.error(error.response.data.message);
-      } else {
-        toast.error("Erro ao encontrar detalhes do prato.");
-      }
+        if (response.error) {
+          toast.error(error.response.data.message);
+        } else {
+          toast.error("Erro ao encontrar detalhes do prato.");
+        }
     }
   }
 
   return (
-    <DishContext.Provider
-      value={{
-        dish,
-        setDish,
-        category,
-        setCategory,
-        ingredientsExists,
-        setIngredientsExists,
-        fetchDishDetails,
-      }}
-    >
+    <DishContext.Provider value={{ dish, setDish, fetchDishDetails }}>
       {children}
     </DishContext.Provider>
   );
