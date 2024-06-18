1171403d3cb6446f4d9dba0d4f05817e075bd8df
Ryan <ryangabriel_cap@hotmail.com>
2024-06-18 22:37:39 +0100

    feat: Adicionado rotas para não usuários poderem verificar o que há dentro do restaurante, porém sem acesso a obter produtos

--- src/routes/auth.routes.jsx
+++ src/routes/auth.routes.jsx
@@ -1,15 +1,34 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
-import { SignIn } from '../pages/SignIn'
-import { SignUp } from '../pages/SignUp'
+import { SignIn } from "../pages/SignIn";
+import { SignUp } from "../pages/SignUp";
+import { Home } from "../pages/Home";
+import { SearchProvider } from "../hooks/Search";
+import { Details } from "../pages/Details";
+import { DishProvider } from "../hooks/Dish";
+import { DefaultLayout } from "../components/Layouts/DefaultLayout";
 
 export function AuthRoutes() {
-  return(
-    <Routes>
-      <Route element={<SignIn/>} path="/"/>
-      <Route element={<SignUp/>} path="/register"/>
+  return (
+    <SearchProvider>
+      <Routes>
+        <Route path="/" element={<DefaultLayout />}>
 
-     <Route path="*" element={<Navigate to="/"/>}/>
-    </Routes>
-  )
+          <Route element={<Home />} path="/" />
+          <Route
+            path="/details/:id"
+            element={
+              <DishProvider>
+                <Details />
+              </DishProvider>
+            }
+          />
+        </Route>
+        <Route element={<SignIn />} path="/login" />
+        <Route element={<SignUp />} path="/register" />
+        <Route path="*" exact={true} element={<Home />} />
+        
+      </Routes>
+    </SearchProvider>
+  );
 }
